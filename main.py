a047fe00e4749029ffa8b63c0472fa80b946026f
Beatriz Niemeyer <bia.niemeyer@hotmail.com>
2023-01-09 16:43:36 -0500

    email working

--- main.py
+++ main.py
@@ -1,12 +1,54 @@
+#!C:\Python27\python.exe
 import smtplib
 from email.message import EmailMessage
 import os
-from flask import Flask, request
+from flask import Flask, redirect, url_for, request, render_template
 
 app = Flask(__name__)
 
 
-def sending_alert(subject, body, to):
+def txt_alert(subject, body, to):
+    msg = EmailMessage()
+    msg.set_content(body)
+    msg["subject"] = subject
+    msg["to"] = [to]
+    msg["from"] = "Your Doctor"
+
+    user = "bianiemeyer150894@gmail.com"
+    password = "sivlwyjeuaxtdhgf"
+
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(user, password)
+    server.send_message(msg)
+    server.quit()
+
+
+@app.route("/")
+def home():
+
+    return render_template("base.html")
+
+
+@app.route("/info")
+def info():
+    name = request.args.get('name')
+    email = request.args.get('email')
+    phone = request.args.get('phone')
+    verizon_phone = phone + "@vtext.com"
+    subject = "Pay"
+    body = f"{name}, pay your bill"
+    to = f"{email}"
+    print(name, phone, verizon_phone, "****************************")
+
+    print("I AM HEEREEEEEEEEE")
+    return redirect(url_for("alert", subject=subject, body=body, to=to, verizon_phone=verizon_phone))
+
+
+@app.route("/<subject>/<body>/<to>/<verizon_phone>")
+def alert(subject, body, to, verizon_phone):
+    print("insideeeeeeee functionnnnnnnnnnn", subject, body, to)
+
     msg = EmailMessage()
     msg.set_content(body)
     msg["subject"] = subject
@@ -23,23 +65,12 @@ def sending_alert(subject, body, to):
     server.send_message(msg)
     server.quit()
 
-
-@app.route('/')
-def alert():
-
-    name = request.args.get("name")
-    email = request.args.get("email")
-    phone = request.args.get("phone")
-    account = request.args.get("account")
-
-    subject = "Your HealthCare Facility - Action Required"
-    body = f"Dear {name},\nYour account {account} has a balance pending. For more information, please access www.yourhealthcare.com to have access your balance and get more information. \nThank you"
-    sending_alert(subject, body, email)
-    sending_alert(subject, body, phone + "@vtext.com")
+    return redirect("/")
 
 
 if __name__ == "__main__":
+    # app.run()
     app.run(host="0.0.0.0", debug=True)
-    # alert("Bill", "Pay your bill", [
-    #     "bia.niemeyer@hotmail.com", "bianiemeyer150894@gmail.com"])
-    # alert("Bill", "Pay your bill", "6094716488@vtext.com")
+
+    # email_alert(name, "Pay your bill", email)
+    # txt_alert("Bill", "Pay your bill", "6094716488@vtext.com")
